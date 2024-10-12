18b969b02f963e6a6d6c521c6e6f02978627a1f4
minhazapon <minhajulabedinapon@gmail.com>
2024-10-12 08:47:44 -0700

    added paginate file

--- src/shop file/Shopdetails.jsx
+++ src/shop file/Shopdetails.jsx
@@ -16,7 +16,13 @@ const Shopdetails = ({ image, company, price, title }) => {
             <h2 className=" text-xl tracking-wide font-serif"> {title} </h2>
             <div className=" flex justify-between items-center ">
              <p  className="  text-xl text-gray-500 font-thin "> {price} </p>
-             <img className="  hover:border-[1px] border-black p-2  h-[40px]" src="https://cdn-icons-png.flaticon.com/128/1124/1124233.png" alt="" /> 
+             <div>
+                <a href="#_" class="relative inline-block px-4 py-2 font-medium group">
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span class="relative text-black group-hover:text-white font-serif ">Add Cart</span>
+                </a>
+             </div>
             </div>
 		    </div>
             </div>
